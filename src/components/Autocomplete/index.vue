de2b202a0d8bf8907d03c3e15fa32d29222b4942
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-10-04 17:10:56 -0400

    Implement first version of custom Vue component: Autocomplete

---
+++ src/components/Autocomplete/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <section class="container-fluid">
+    <typeahead ref="typeahead2"
+      v-model="modelVar"
+      :async-src="'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(modelVar) + '.json?country=us&access_token=pk.eyJ1IjoiZmNjIiwiYSI6IktmWXZja00ifQ.GEwhTOZ-sw8BSJK7Q17S_Q&'"
+      async-key="features"
+      item-key="place_name"
+      :ignore-case="ignoreCase"
+      :match-start="matchStart"
+      :force-select="false"
+      :open-on-focus="openOnFocus"
+      >
+      <input data-role="input" class="form-control" type="text">
+    </typeahead>
+    <br/>
+    <div class="alert alert-info">
+      <p v-if="modelVar">You selected: {{modelVar}}</p>
+      <p v-else>Please select an item.</p>
+    </div>
+  </section>
+</template>
+
+<script>
+  // Include UIV library's Typeahead component
+  import { Typeahead } from 'uiv'
+
+  export default {
+    components: {
+      Typeahead: Typeahead
+    },
+    data () {
+      return {
+        modelVar: '',
+        itemKey: 'name',
+        ignoreCase: true,
+        matchStart: false,
+        openOnFocus: false
+      }
+    }
+  }
+</script>
