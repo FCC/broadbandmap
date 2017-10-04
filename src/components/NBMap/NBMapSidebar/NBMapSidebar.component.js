3973c49d0d68d4dd7dfac30164626765f94dddd7
bizui <bizuilize@gmail.com>
2017-10-04 10:55:00 -0400

    [GISNBM2-13] Add collapsible map sidebar

--- src/components/NBMap/NBMapSidebar/NBMapSidebar.component.js
+++ src/components/NBMap/NBMapSidebar/NBMapSidebar.component.js
@@ -1,20 +1,31 @@
+import { Tooltip } from 'uiv'
 import EventHub from '../../../_mixins/EventHub.js'
 
 export default {
   name: 'nbMapSidebar',
+  components: { Tooltip },
   props: [],
   mounted () {
 
   },
   data () {
     return {
-      hidePane: false
+      hidePane: false,
+      toggleText: 'Collapse sidebar',
+      toggleTooltip: true
     }
   },
   methods: {
-    toggle: function () {
-      this.hidePane = !this.hidePane
+    toggle: function (event) {
+      let thisTooltip = document.querySelectorAll('.tooltip')
+      
+      for(let i=0; i<thisTooltip.length; i++) {
+        thisTooltip[i].parentNode.removeChild(thisTooltip[i])
+      }
 
+      this.hidePane = !this.hidePane
+      this.toggleText = this.hidePane ? 'Expand sidebar' :'Collapse sidebar'
+            
       EventHub.$emit('toggleSidebar', this.hidePane)
     }
   },
