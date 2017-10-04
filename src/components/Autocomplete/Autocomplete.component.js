220965b54dd6fb464f9e3d78d9f265121c2815ee
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-10-04 18:18:44 -0400

    Split the Autocomplete component's template (HTML) and JS out into separate files

---
+++ src/components/Autocomplete/Autocomplete.component.js
@@ -0,0 +1,18 @@
+// Include UIV library's Typeahead component
+import { Typeahead } from 'uiv'
+
+export default {
+components: {
+    Typeahead: Typeahead
+},
+props: ['placeholderText'],
+    data () {
+        return {
+        modelVar: '',
+        itemKey: 'name',
+        ignoreCase: true,
+        matchStart: false,
+        openOnFocus: false
+        }
+    }
+}
