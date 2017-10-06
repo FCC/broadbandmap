ae1e8895670b5cfdc2a55a3f0a0f2443601bf7eb
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-10-05 21:31:00 -0400

    When user select an address, redirect browser to Area Summary page. Pass lat,lon & address in URL. Simplify code & add comments.

--- src/components/Autocomplete/Autocomplete.component.js
+++ src/components/Autocomplete/Autocomplete.component.js
@@ -1,18 +1,27 @@
-// Include UIV library's Typeahead component
+// Include the needed "uiv" library components
 import { Typeahead } from 'uiv'
 
 export default {
-components: {
-    Typeahead: Typeahead
-},
+    components: {
+        Typeahead: Typeahead
+    },
     props: ['placeholderText'],
     data () {
         return {
-            modelVar: '',
-            itemKey: 'name',
-            ignoreCase: true,
-            matchStart: false,
-            openOnFocus: false
+            // TYPEAHEAD vars
+            typeaheadModel: '',
+            itemKey: 'name'
+        }
+    },
+    methods: {
+        // Called when the user selects an address returned by the geocoder
+        addressClicked (event, props, item) {
+            // Select the item the user clicked
+            props.select(item);
+            // Create the URL
+            const newUrl = "location-summary?lat=" + item.center[1] + "&lon=" + item.center[0] + "&place_name=" + encodeURIComponent(item.place_name);
+            // Push the URL to the Vue router
+            this.$router.push(newUrl);
         }
     }
 }
--- src/components/Autocomplete/index.vue
+++ src/components/Autocomplete/index.vue
@@ -1,2 +1,6 @@
+<!--
+Receives these variables:
+    placeholderText - For the input field
+-->
 <template src="./Autocomplete.component.html"></template>
 <script src="./Autocomplete.component.js"></script>
