37bd4620509c5b0cb979a504c3380b60c255cb79
bizui <bizuilize@gmail.com>
2017-10-06 12:05:34 -0400

    add map layer switch control

--- src/components/NBMap/layers-location.js
+++ src/components/NBMap/layers-location.js
@@ -5,11 +5,17 @@ export default {
     'mapbox:autocomposite': true
   },
   sources: {
-    mapbox: {
-      url: 'mapbox://mapbox.mapbox-streets-v7',
-      type: 'vector'
+    dark: {
+      type: 'raster',
+      url: 'mapbox://mapbox.dark',
+      tileSize: 256
     },
-    baseStreet: {
+    satellite: {
+      type: 'raster',
+      url: 'mapbox://mapbox.satellite',
+      tileSize: 256
+    },
+    street: {
       type: 'raster',
       url: 'mapbox://fcc.k74ed5ge',
       tileSize: 256
@@ -27,29 +33,34 @@ export default {
         'https://geo.fcc.gov/fcc/gwc/service/wms?tiled=true&service=WMS&request=GetMap&layers=bpr_apr2017_county_layer_nonfixed&styles=bpr_layer_fixed_0&format=image%2Fpng&transparent=true&version=1.1.1&color=%236CBCD5&height=256&width=256&bbox={bbox-epsg-3857}&srs=EPSG%3A3857'
       ],
       tileSize: 256
-    },
-    tribal: {
-      type: 'raster',
-      tiles: [
-        'https://geo.fcc.gov/fcc/gwc/service/wms?tiled=true&service=WMS&request=GetMap&layers=bpr_tribal&styles=bpr_tribal&format=image%2Fpng&transparent=true&version=1.1.1&color=%236CBCD5&height=256&width=256&bbox={bbox-epsg-3857}&srs=EPSG%3A3857'
-      ],
-      tileSize: 256
-    },
-    urban: {
-      type: 'raster',
-      tiles: [
-        'https://geo.fcc.gov/fcc/gwc/service/wms?tiled=true&service=WMS&request=GetMap&layers=fcc:bpr_county_layer_urban_only&styles=bpr_layer_urban&format=image%2Fpng&transparent=true&version=1.1.1&color=%236CBCD5&height=256&width=256&bbox={bbox-epsg-3857}&srs=EPSG%3A3857'
-      ],
-      tileSize: 256
     }
   },
   sprite: 'mapbox://sprites/mapbox/basic-v8',
   glyphs: 'mapbox://fonts/mapbox/{fontstack}/{range}.pbf',
   layers: [
     {
-      id: 'base-street',
+      id: 'dark',
       type: 'raster',
-      source: 'baseStreet'
+      source: 'dark',
+      layout: {
+        visibility: 'visible'
+      }
+    },
+    {
+      id: 'satellite',
+      type: 'raster',
+      source: 'satellite',
+      layout: {
+        visibility: 'none'
+      }
+    },
+    {
+      id: 'street',
+      type: 'raster',
+      source: 'street',
+      layout: {
+        visibility: 'none'
+      }
     },
     {
       id: 'deploymentFixed',
@@ -66,24 +77,6 @@ export default {
       source: 'deploymentNoFixed',
       layout: {
         visibility: 'visible'
-      },
-      paint: {}
-    },
-    {
-      id: 'tribal',
-      type: 'raster',
-      source: 'tribal',
-      layout: {
-        visibility: 'none'
-      },
-      paint: {}
-    },
-    {
-      id: 'urban',
-      type: 'raster',
-      source: 'urban',
-      layout: {
-        visibility: 'none'
       },
       paint: {}
     }
