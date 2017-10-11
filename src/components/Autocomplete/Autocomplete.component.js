9d8b03f5888a20f8b53003f68e0a2df4c417ef36
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-10-11 14:48:50 -0400

    Switch from our modified Typeahead component back to the uiv library's, which now supports disabling client-side filtering of geocoded addresses

--- src/components/Autocomplete/Autocomplete.component.js
+++ src/components/Autocomplete/Autocomplete.component.js
@@ -1,7 +1,5 @@
 // Include the needed "uiv" library components
-// import { Typeahead } from 'uiv'
-// This is a modified fork off of the Typeahead component from the uiv library v0.11.10
-import Typeahead from './Typeahead.vue'
+import { Typeahead } from 'uiv'
 
 export default {
     components: {
