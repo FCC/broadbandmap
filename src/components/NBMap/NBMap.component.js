a7054d86eb06f8b6598a41644b2a048573f2432c
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-10-18 00:42:49 -0400

    Add isValidAddress()

--- src/components/NBMap/NBMap.component.js
+++ src/components/NBMap/NBMap.component.js
@@ -195,6 +195,9 @@ export default {
     },
     isValidLatLon () {
       return (typeof this.$route.query.lat === 'string' && typeof this.$route.query.lon === 'string' && this.$route.query.lat.length && this.$route.query.lon.length && !isNaN(this.$route.query.lat) && !isNaN(this.$route.query.lon))
+    },
+    isValidAddress () {
+      return (typeof this.$route.query.place_name === 'string' && this.$route.query.place_name.length)
     }
   },
   computed: {
