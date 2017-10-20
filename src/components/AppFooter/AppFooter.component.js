ab1298576acd7de371c321c004fc30d075d7269c
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-10-20 18:30:15 -0400

    Implement uiv Modal component in app footer, using existing Vue Event Hub to make it available throughout app

--- src/components/AppFooter/AppFooter.component.js
+++ src/components/AppFooter/AppFooter.component.js
@@ -1,13 +1,30 @@
+import EventHub from '../../_mixins/EventHub.js'
+
+import { Modal } from 'uiv'
 export default {
   name: 'AppFooter',
+  components: {
+    Modal: Modal
+  },
   props: [],
   mounted () {
 
   },
   data () {
     return {
-
+      modalTitle: '',
+      modalBody: '',
+      modalModel: false
     }
+  },
+  created () {
+    EventHub.$on('openModal', function (title, body) {
+      // Display Modal uiv component
+      this.modalTitle = title
+      this.modalBody = body
+      this.modalModel = true
+    // Make "this" available here
+    }.bind(this))
   },
   methods: {
 
