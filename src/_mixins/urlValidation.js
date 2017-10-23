81daf4966469ad43ca81c5820fdf717eea77df3f
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-10-23 16:01:55 -0400

    Validate latitude & longitude ranges

--- src/_mixins/urlValidation.js
+++ src/_mixins/urlValidation.js
@@ -5,7 +5,8 @@ export const urlValidation = {
   },
   methods: {
     isValidLatLon (lat, lon) {
-      return (typeof lat === 'string' && typeof lon === 'string' && lat.length && lon.length && !isNaN(lat) && !isNaN(lon))
+      // NOTE: Mapbox intermittently rejects a latitude of exactly -90
+      return (typeof lat === 'string' && typeof lon === 'string' && lat.length && lon.length && !isNaN(lat) && !isNaN(lon) && lat > -90 && lat <= 90 && lon >= -180 && lon <= 180)
     },
     isValidAddress (address) {
       return (typeof address === 'string' && address.length)
