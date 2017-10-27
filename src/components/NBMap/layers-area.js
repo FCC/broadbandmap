b936dfd718ecfb1698301260fa88ad02b8df07dd
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-10-27 15:28:33 -0400

    Area Summary: Remove old BPR layers deploymentFixed and deploymentNoFixed

--- src/components/NBMap/layers-area.js
+++ src/components/NBMap/layers-area.js
@@ -22,20 +22,6 @@ export default {
     county: {
       type: 'vector',
       url: 'mapbox://fcc.ao2kqazm'
-    },
-    deploymentFixed: {
-      type: 'raster',
-      tiles: [
-        'https://geo.fcc.gov/fcc/gwc/service/wms?tiled=true&service=WMS&request=GetMap&layers=bpr_apr2017_county_layer_fixed&styles=bpr_layer_fixed_1&format=image%2Fpng&transparent=true&version=1.1.1&color=%236CBCD5&height=256&width=256&bbox={bbox-epsg-3857}&srs=EPSG%3A3857'
-      ],
-      tileSize: 256
-    },
-    deploymentNoFixed: {
-      type: 'raster',
-      tiles: [
-        'https://geo.fcc.gov/fcc/gwc/service/wms?tiled=true&service=WMS&request=GetMap&layers=bpr_apr2017_county_layer_nonfixed&styles=bpr_layer_fixed_0&format=image%2Fpng&transparent=true&version=1.1.1&color=%236CBCD5&height=256&width=256&bbox={bbox-epsg-3857}&srs=EPSG%3A3857'
-      ],
-      tileSize: 256
     }
   },
   sprite: 'mapbox://sprites/mapbox/basic-v8',
@@ -77,24 +63,6 @@ export default {
       paint: {
         'line-color': '#74994e'
       }
-    },
-    {
-      id: 'deploymentFixed',
-      type: 'raster',
-      source: 'deploymentFixed',
-      layout: {
-        visibility: 'visible'
-      },
-      paint: {}
-    },
-    {
-      id: 'deploymentNoFixed',
-      type: 'raster',
-      source: 'deploymentNoFixed',
-      layout: {
-        visibility: 'visible'
-      },
-      paint: {}
     }
   ]
 }
