d34e57367c3beea91e4b1d49b427d92375a438f3
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-11-07 18:24:06 -0500

    Location Summary & Area Summary pages: Load 2010 Census tracts, not 2016 tracts

--- src/components/NBMap/layers-area.js
+++ src/components/NBMap/layers-area.js
@@ -60,7 +60,7 @@ export default {
       id: 'tract',
       type: 'line',
       source: 'tract',
-      'source-layer': 'nbm2_tract2016geojson',
+      'source-layer': 'nbm2_tract2010geojson',
       layout: {
         visibility: 'visible'
       },
