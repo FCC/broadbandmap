36ba3c42dc4d0e8a7a05beda01dac9ff3eb132d9
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-11-08 02:27:33 -0500

    Autocomplete: Make Ajax call to Socrata API to search 6 geographies in lookup table & populate typeahead

--- src/components/Autocomplete/Autocomplete.component.js
+++ src/components/Autocomplete/Autocomplete.component.js
@@ -1,8 +1,8 @@
+import axios from 'axios'
 // Include the needed "uiv" library components
 import { Typeahead } from 'uiv'
 import { urlValidation } from '../../_mixins/urlValidation.js'
 import EventHub from '../../_mixins/EventHub.js'
-
 export default {
   components: {
     Typeahead: Typeahead
@@ -31,11 +31,12 @@ export default {
     },
     // Called when user pressed enter or clicked search
     gotoGeography (event) {
+      let newURL = ''
       switch (this.searchType) {
         case 'Address':
           if (typeof this.typeaheadModel === 'object' && typeof this.typeaheadModel.id === 'string') {
             // Create the URL
-            let newURL = 'location-summary?lat=' + this.typeaheadModel.center[1] + '&lon=' + this.typeaheadModel.center[0] + '&place_name=' + encodeURIComponent(this.typeaheadModel.place_name)
+            newURL = 'location-summary?lat=' + this.typeaheadModel.center[1] + '&lon=' + this.typeaheadModel.center[0] + '&place_name=' + encodeURIComponent(this.typeaheadModel.place_name)
             this.$router.push(newURL)
           } else {
             // Call Modal component in app footer
@@ -45,15 +46,45 @@ export default {
         case 'Coordinates':
           let coordinatesArray = this.typeaheadModel.split(',')
           if (coordinatesArray.length === 2 && this.isValidLatLon(coordinatesArray[0], coordinatesArray[1])) {
-            let newURL = 'location-summary?lat=' + coordinatesArray[0].trim() + '&lon=' + coordinatesArray[1].trim()
+            newURL = 'location-summary?lat=' + coordinatesArray[0].trim() + '&lon=' + coordinatesArray[1].trim()
             this.$router.push(newURL)
           } else {
             // Call Modal component in app footer
             EventHub.$emit('openModal', 'No results found', 'Please enter valid coordinates in "latitude, longitude" format.')
           }
           break
+        case 'State':
+          console.log('gotoGeography(), searchType= ' + this.searchType + ', typeaheadModel= ', this.typeaheadModel)
+          newURL = 'area-summary?type=state&geoid=' + this.typeaheadModel.geoid + '&bbox=' + this.typeaheadModel.bbox_arr
+          this.$router.push(newURL)
+          break
+        case 'CBSA (MSA)':
+          console.log('gotoGeography(), searchType= ' + this.searchType + ', typeaheadModel= ', this.typeaheadModel)
+          newURL = 'area-summary?type=cbsa&geoid=' + this.typeaheadModel.geoid + '&bbox=' + this.typeaheadModel.bbox_arr
+          this.$router.push(newURL)
+          break
+        case 'County':
+          console.log('gotoGeography(), searchType= ' + this.searchType + ', typeaheadModel= ', this.typeaheadModel)
+          newURL = 'area-summary?type=county&geoid=' + this.typeaheadModel.geoid + '&bbox=' + this.typeaheadModel.bbox_arr
+          this.$router.push(newURL)
+          break
+        case 'Congressional District':
+          console.log('gotoGeography(), searchType= ' + this.searchType + ', typeaheadModel= ', this.typeaheadModel)
+          newURL = 'area-summary?type=district&geoid=' + this.typeaheadModel.geoid + '&bbox=' + this.typeaheadModel.bbox_arr
+          this.$router.push(newURL)
+          break
+        case 'Tribal Area':
+          console.log('gotoGeography(), searchType= ' + this.searchType + ', typeaheadModel= ', this.typeaheadModel)
+          newURL = 'area-summary?type=tribal&geoid=' + this.typeaheadModel.geoid + '&bbox=' + this.typeaheadModel.bbox_arr
+          this.$router.push(newURL)
+          break
+        case 'Census Place':
+          console.log('gotoGeography(), searchType= ' + this.searchType + ', typeaheadModel= ', this.typeaheadModel)
+          newURL = 'area-summary?type=place&geoid=' + this.typeaheadModel.geoid + '&bbox=' + this.typeaheadModel.bbox_arr
+          this.$router.push(newURL)
+          break
         default:
-          console.log('DEBUG: No handler for searchType = ' + this.searchType)
+          console.log('DEBUG: No handler in gotoGeography() for searchType = ' + this.searchType)
       }
     },
     // Called by data() on init, and when searchType changes
@@ -73,7 +104,69 @@ export default {
         this.itemKey = ''
         */
         this.typeaheadModel = this.isValidLatLon(this.$route.query.lat, this.$route.query.lon) ? this.$route.query.lat + ', ' + this.$route.query.lon : ''
+      } else if (this.searchType === 'State') {
+        this.itemKey = 'name'
+        this.asyncKey = ''
+        this.fetchLookupTable('state')
+      } else if (this.searchType === 'CBSA (MSA)') {
+        this.itemKey = 'name'
+        this.asyncKey = ''
+        this.fetchLookupTable('cbsa')
+      } else if (this.searchType === 'Tribal Area') {
+        this.itemKey = 'name'
+        this.asyncKey = ''
+        this.fetchLookupTable('tribal')
+      } else if (this.searchType === 'Census Place') {
+        this.itemKey = 'name'
+        this.asyncKey = ''
+        this.fetchLookupTable('place')
+      } else if (this.searchType === 'Congressional District') {
+        this.itemKey = 'name'
+        this.asyncKey = ''
+        this.fetchLookupTable('cd')
+      } else if (this.searchType === 'County') {
+        this.itemKey = 'name'
+        this.asyncKey = ''
+        this.fetchLookupTable('county')
+      } else {
+        console.log('DEBUG: No handler in Autocomplete for searchType= ', this.searchType)
       }
+    },
+    // Call Socrata API - Lookup Table for geographies
+    fetchLookupTable (fetchType) {
+      const self = this
+      axios
+      .get('https://fcc-dev.data.socrata.com/resource/rz8j-sgij.json', {
+        params: {
+          type: fetchType,
+          $limit: 50000,
+          $order: 'name',
+          $$app_token: process.env.SOCRATA_APP_TOKEN
+        },
+        headers: {
+          // Dev: Authentication to Socrata using HTTP Basic Authentication
+          'Authorization': 'Basic ' + process.env.SOCRATA_HTTP_BASIC_AUTHENTICATION
+        }
+      })
+      .then(function (response) {
+        console.log('Socrata response= ', response)
+        self.dataSource = response.data
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // Server responded with a status code that falls out of the range of 2xx
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        } else if (error.request) {
+          // Request was made but no response was received
+          console.log(error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message)
+        }
+        console.log(error)
+      })
     }
   },
   watch: {
