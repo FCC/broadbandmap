21497a91a3b99e80befbd09c5abe353c98d4b180
Steven Greenwaters <steven.greenwaters@fcc.gov>
2017-11-07 21:55:50 -0500

    Area Summary: Make Ajax call to Socrata server to get "combined" table (requires new .env file for dev)

--- src/components/AreaSummary/AreaSummary.component.js
+++ src/components/AreaSummary/AreaSummary.component.js
@@ -1,17 +1,49 @@
 import nbMap from '../NBMap/'
 import nbMapSidebar from '../NBMap/NBMapSidebar/'
+import axios from 'axios'
 
 export default {
   name: 'AreaSummary',
   components: { 'nbMap': nbMap, 'nbMapSidebar': nbMapSidebar },
   props: [],
-  mounted () {
-
-  },
   data () {
     return {
 
     }
+  },
+  mounted () {
+    // Call Socrata API - Combined Table for charts
+    axios
+    .get('https://fcc-dev.data.socrata.com/resource/wnht-yxes.json', {
+      params: {
+        id: 51013,
+        type: 'county',
+        tech: 'a',
+        $$app_token: process.env.SOCRATA_APP_TOKEN
+      },
+      headers: {
+        // Dev: Authentication to Socrata using HTTP Basic Authentication
+        'Authorization': 'Basic ' + process.env.SOCRATA_HTTP_BASIC_AUTHENTICATION
+      }
+    })
+    .then(function (response) {
+      console.log('Success! response= ', response)
+    })
+    .catch(function (error) {
+      if (error.response) {
+        // Server responded with a status code that falls out of the range of 2xx
+        console.log(error.response.data)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+      } else if (error.request) {
+        // Request was made but no response was received
+        console.log(error.request)
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log('Error', error.message)
+      }
+      console.log(error)
+    })
   },
   methods: {
 
