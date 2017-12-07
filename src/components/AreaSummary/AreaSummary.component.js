ae71853acde6d431134518f00ea24e9b71607d7c
bizui <bizuilize@gmail.com>
2017-12-07 13:35:07 -0500

    remove D3 chart

--- src/components/AreaSummary/AreaSummary.component.js
+++ src/components/AreaSummary/AreaSummary.component.js
@@ -9,8 +9,6 @@ import EventHub from '../../_mixins/EventHub.js'
 import { urlValidation } from '../../_mixins/urlValidation.js'
 import { updateMapLayers } from '../../_mixins/map-update-layers.js'
 
-const d3 = require('d3')
-
 export default {
   name: 'AreaSummary',
   components: { Carousel, Slide, nbMap, nbMapSidebar, StackedBarChart },
@@ -142,7 +140,6 @@ export default {
       })
       .then(function (response) {
         // console.log('Socrata response= ', response)
-        self.drawCombinedCharts(response)
       })
       .catch(function (error) {
         if (error.response) {
@@ -159,117 +156,6 @@ export default {
         }
         console.log(error)
       })
-    },
-    drawCombinedCharts (response) {
-      var data = response.data
-      let totalPop = 0
-      for (var index in data) {
-        // Convert populations from strings to numbers
-        data[index].has_0 = parseInt(data[index].has_0)
-        data[index].has_1 = parseInt(data[index].has_1)
-        data[index].has_2 = parseInt(data[index].has_2)
-        data[index].has_3plus = parseInt(data[index].has_3plus)
-        // Convert to percentages
-        totalPop = data[index].has_0 + data[index].has_1 + data[index].has_2 + data[index].has_3plus
-        data[index].has_0 = data[index].has_0 / totalPop * 100
-        data[index].has_1 = data[index].has_1 / totalPop * 100
-        data[index].has_2 = data[index].has_2 / totalPop * 100
-        data[index].has_3plus = data[index].has_3plus / totalPop * 100
-      }
-
-      // Draw charts using the D3 library
-      var svg = d3.select('svg')
-      var margin = {top: 20, right: 20, bottom: 30, left: 40}
-      var width = +svg.attr('width') - margin.left - margin.right
-      var height = +svg.attr('height') - margin.top - margin.bottom
-      var g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')')
-
-      var x = d3.scaleBand()
-        .rangeRound([0, width])
-        .paddingInner(0.05)
-        .align(0.1)
-
-      var y = d3.scaleLinear()
-        .rangeRound([height, 0])
-
-      var z = d3.scaleOrdinal()
-        .range(['#e6eecf', '#9bc4c1', '#68a8b7', '#2e557a'])
-
-      var keys = ['has_0', 'has_1', 'has_2', 'has_3plus']
-      /* Do not sort
-      data.sort(function(a, b) { return b.total - a.total; })
-      */
-      // X-domain
-      x.domain(data.map(function (d) {
-        return d.speed
-      }))
-      /* Use 100% instead
-      y.domain([0, d3.max(data, function(d) { return d.total; })]).nice()
-      */
-      // Y-domain - Set range from 0% to 100%
-      y.domain([0, 100]).nice()
-      // Z-domain (Is this used?)
-      z.domain(keys)
-
-      // Bars
-      g.append('g')
-        .selectAll('g')
-        .data(d3.stack().keys(keys)(data))
-        .enter().append('g')
-        .attr('fill', function (d) { return z(d.key) })
-        .selectAll('rect')
-        .data(function (d) { return d })
-        .enter().append('rect')
-        // .attr('x', function (d) { return x(d.data.speed) })
-        .attr('x', function (d) {
-          // console.log('d=', d)
-          // console.log('x(d)=', x(d))
-          return x(d.data.speed)
-        })
-        .attr('y', function (d) { return y(d[1]) })
-        .attr('height', function (d) { return y(d[0]) - y(d[1]) })
-        .attr('width', x.bandwidth())
-        // .attr('width', 20)
-
-      // X-axis
-      g.append('g')
-        .attr('class', 'axis')
-        .attr('transform', 'translate(0,' + height + ')')
-        .call(d3.axisBottom(x))
-
-      // Y-axis
-      g.append('g')
-        .attr('class', 'axis')
-        .call(d3.axisLeft(y).ticks(null, 's'))
-        .append('text')
-        .attr('x', 2)
-        .attr('y', y(y.ticks().pop()) + 0.5 - 10)
-        .attr('dy', '0.32em')
-        .attr('fill', '#000')
-        .attr('font-weight', 'bold')
-        .attr('text-anchor', 'start')
-        .text('% of population')
-
-      /* Legend:
-      var legend = g.append('g')
-        .attr('font-family', 'sans-serif')
-        .attr('font-size', 10)
-        .attr('text-anchor', 'end')
-        .selectAll('g')
-        .data(keys.slice().reverse())
-        .enter().append('g')
-        .attr('transform', function(d, i) { return 'translate(0,' + i * 20 + ')'; });
-      legend.append('rect')
-        .attr('x', width - 19)
-        .attr('width', 19)
-        .attr('height', 19)
-        .attr('fill', z);
-      legend.append('text')
-        .attr('x', width - 24)
-        .attr('y', 9.5)
-        .attr('dy', '0.32em')
-        .text(function(d) { return d; });
-      */
     }
   },
   computed: {
