5b3bf3b33764cac76e1a2e14e44d903358168b88
Alexandre Efremov <alexandre.efremov@fcc.gov>
2017-12-13 13:19:22 -0500

    Added table settings component

--- src/components/AreaComparison/AreaComparison.component.js
+++ src/components/AreaComparison/AreaComparison.component.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import { Dropdown, Tooltip } from 'uiv'
 
+import EventHub from '../../_mixins/EventHub.js'
 import Autocomplete from '@/components/Autocomplete/index.vue'
 import BookmarkLink from '@/components/BookmarkLink/'
 import searchGeogTypes from '../../_mixins/search-geog-types.js'
@@ -15,6 +16,16 @@ export default {
   mounted () {
     this.searchLabel = 'County'
     this.searchOptsList = this.searchTypes.comparison
+
+    EventHub.$on('updateTableSettings', function (selectedTech, selectedSpeed) {
+      console.log('On updateTableSettings')
+      this.updateTechSpeed(selectedTech, selectedSpeed)
+    }.bind(this))
+    EventHub.$on('removeTableData', (propertyID, removeAll) => this.removeData(propertyID, removeAll))
+  },
+  destroyed () {
+    EventHub.$off('updateTableSettings')
+    EventHub.$off('removeTableData')
   },
   data () {
     return {
@@ -32,7 +43,17 @@ export default {
       this.$refs.autocomplete1.typeaheadModel = ''
     },
     searchArea: function (areaType) { // Set the search area input value to nationwide or blank
+      console.log(areaType)
       this.$refs.autocomplete2.typeaheadModel = areaType
+    },
+    openTableSettings () {
+      EventHub.$emit('openTableSettings')
+    },
+    updateTechSpeed (selectedTech, selectedSpeed) {
+      console.log('inside updateTechSpeed', selectedTech, selectedSpeed)
+    },
+    removeData(propertyID, removeAll) {
+      console.log('inside removeData', propertyID, removeAll)
     }
   },
   computed: {
