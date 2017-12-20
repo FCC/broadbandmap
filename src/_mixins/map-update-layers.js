3fbd3a242d8be0da985fe5bb9788702d3ba2e7c6
bizui <bizuilize@gmail.com>
2017-12-19 20:37:40 -0500

    updated URL parameters when selectedTech and Speed change

--- src/_mixins/map-update-layers.js
+++ src/_mixins/map-update-layers.js
@@ -88,14 +88,23 @@ export const updateMapLayers = {
           vm.Map.removeLayer(layer.id)
         }
       }
+
+      // Set selected Tech and Speed to empty when all layers are removed
+      if (removeAll) {
+        this.selectedTech = ''
+        this.selectedSpeed = ''
+        this.updateURLParams()
+      }
     },
-    // Called by mounted() and Map.on('load')
     updateTechSpeed (selectedTech, selectedSpeed) { // e.g. acfosw, 25_3
       let propertyID = [selectedTech, selectedSpeed].join('_')
 
       // When base layer style is changed, the selected tech & speed layers will be reloaded
       this.selectedTech = selectedTech
       this.selectedSpeed = selectedSpeed
+
+      // Update URL params when selected Tech and Speed change
+      this.updateURLParams()
 
       // Add layer sources if they don't exist already
       if (this.Map.getSource('25_3') === undefined) {
