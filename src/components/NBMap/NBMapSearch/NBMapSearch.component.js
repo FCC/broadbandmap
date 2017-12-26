d2be00f9e981a197a14d7313bf4f8f710fa1f596
bizui <bizuilize@gmail.com>
2017-12-26 16:42:48 -0500

    clear Search input field if place_name undefined

--- src/components/NBMap/NBMapSearch/NBMapSearch.component.js
+++ src/components/NBMap/NBMapSearch/NBMapSearch.component.js
@@ -106,10 +106,14 @@ export default {
     // When query params change for the same route (URL slug)
     '$route' (to, from) {
       this.receiveSearchType()
-
+      console.log(this.searchType)
       // Update search input field value with place_name
-      if (to.query.place_name !== undefined) {
-        this.$refs.autocomplete2.typeaheadModel = to.query.place_name
+      if (this.searchType === 'Address') {
+        if (to.query.place_name !== undefined) {
+          this.$refs.autocomplete2.typeaheadModel = to.query.place_name
+        } else {
+          this.$refs.autocomplete2.typeaheadModel = ''
+        }
       }
 
       // Update search input field value with lat, lon
