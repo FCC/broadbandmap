2265a1e494c015d35aac593b41db9adc6bcd45e1
bizui <bizuilize@gmail.com>
2017-12-28 18:40:17 -0500

    add new speed layers

--- src/components/NBMap/layers-techSpeed.js
+++ src/components/NBMap/layers-techSpeed.js
@@ -5,6 +5,11 @@ export const sourcesTechSpeed = [
     type: 'vector'
   },
   {
+    id: '4_1',
+    url: 'mapbox://fcc.5p7gp0op',
+    type: 'vector'
+  },
+  {
     id: '10_1',
     url: 'mapbox://fcc.97d0gam3',
     type: 'vector'
@@ -14,42 +19,59 @@ export const sourcesTechSpeed = [
     url: 'mapbox://fcc.4ai93o1d',
     type: 'vector'
   },
-  {
-    id: '50_5',
-    url: 'mapbox://fcc.dcc1b9rv',
-    type: 'vector'
-  },
+
   {
     id: '100_10',
     url: 'mapbox://fcc.btj2xg09',
+    type: 'vector'
+  },
+  {
+    id: '250_25',
+    url: 'mapbox://fcc.7vjqng1m',
+    type: 'vector'
+  },
+  {
+    id: '1000_100',
+    url: 'mapbox://fcc.4tg286y5',
     type: 'vector'
   }
 ]
 
 export const layersTechSpeed = {
   '200': {
-    id: 'all_200',
+    id: 'jun16_10feb17_200',
     source: '200',
     beforeLayer: 'block'
   },
+  '4': {
+    id: 'jun16_10feb17_4',
+    source: '4_1',
+    beforeLayer: 'block'
+  },
   '10': {
-    id: 'all_10',
+    id: 'jun16_10feb17_10',
     source: '10_1',
     beforeLayer: 'block'
   },
   '25': {
-    id: 'all_25',
+    id: 'jun16_10feb17_25',
     source: '25_3',
     beforeLayer: 'block'
   },
-  '50': {
-    id: 'all_50',
-    source: '50_5',
+
+  '100': {
+    id: 'jun16_10feb17_100',
+    source: '100_10',
     beforeLayer: 'block'
   },
-  '100': {
-    id: 'all_100',
-    source: '100_10',
+  '250': {
+    id: 'jun16_10feb17_250',
+    source: '250_25',
+    beforeLayer: 'block'
+  },
+  '1000': {
+    id: 'jun16_10feb17_1000',
+    source: '1000_100',
     beforeLayer: 'block'
   }
 }
