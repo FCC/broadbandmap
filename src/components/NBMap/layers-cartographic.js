ea23e035c4037653efa3f05e7d1834ad6894b27c
Nathaniel Guieb <bizuilize@gmail.com>
2018-01-10 19:24:43 -0500

    remove county and block layers

--- src/components/NBMap/layers-cartographic.js
+++ src/components/NBMap/layers-cartographic.js
@@ -3,37 +3,6 @@
 
 export const LayersCartographic = [
   {
-    id: 'block',
-    source: {
-      type: 'vector',
-      url: 'mapbox://fcc.9tcqhtt6'
-    },
-    type: 'line',
-    'source-layer': 'nbm2_block2010geojson',
-    layout: {
-      visibility: 'visible'
-    },
-    paint: {
-      'line-color': '#484896'
-    }
-  },
-  {
-    id: 'county',
-    source: {
-      type: 'vector',
-      url: 'mapbox://fcc.ao2kqazm'
-    },
-    type: 'line',
-    'source-layer': 'nbm2_county2016geojson',
-    layout: {
-      visibility: 'visible'
-    },
-    paint: {
-      'line-color': '#74994e'
-    },
-    maxzoom: 10
-  },
-  {
     id: 'county-highlighted',
     source: {
       type: 'vector',
