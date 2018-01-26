5ec64acaf9a365624a0a819ff405ae2c6051025e
Nathaniel Guieb <bizuilize@gmail.com>
2018-01-26 14:23:46 -0500

    add download data feature; fix State results table

--- src/components/AreaComparison/AreaComparison.component.js
+++ src/components/AreaComparison/AreaComparison.component.js
@@ -1,6 +1,7 @@
 import { Spinner } from 'spin.js'
 import axios from 'axios'
 import { Dropdown, Tooltip } from 'uiv'
+import FileSaver from 'file-saver'
 
 import EventHub from '../../_mixins/EventHub.js'
 import Autocomplete from '@/components/Autocomplete/index.vue'
@@ -46,6 +47,7 @@ export default {
       }],
       rows: [],
       nwData: [],
+      stateData: [],
       searchType: 'County',
       refreshingDropdown: false,
       selectedTech: 'acfosw',
@@ -84,10 +86,12 @@ export default {
       },
       speedDictionary: {
         '200': '0.2',
+        '4_1': '4',
         '10_1': '10',
         '25_3': '25',
-        '50_5': '50',
-        '100_10': '100'
+        '100_10': '100',
+        '250_25': '250',
+        '1000_100': '1000'
       },
       stateGeoidToName: {},
       stateNameToGeoid: {},
@@ -218,9 +222,9 @@ export default {
         this.toggleSearchType('County')
       }
 
-      if (areaType === '') {
+      if (areaType === 'state') {
         this.searchOptsList = Object.assign({}, this.searchTypes.comparison)
-        this.toggleSearchType('County')
+        // this.toggleSearchType('County')
         delete this.searchOptsList['State']
         delete this.searchOptsList['Tribal Area']
       } else {
@@ -228,7 +232,8 @@ export default {
       }
 
       this.refreshingDropdown = false
-      this.$refs.autocomplete.typeaheadModel = areaType
+
+      this.$refs.autocomplete.typeaheadModel = areaType === 'Nationwide' ? areaType : ''
     },
     openTableSettings () {
       EventHub.$emit('openTableSettings')
@@ -357,7 +362,48 @@ export default {
         headers: this.httpHeaders
       })
       .then(function (response) {
-        self.assembleRows(response.data)
+        self.assembleRows(response.data, 'Nationwide')
+
+        self.nwDataDownload = response.request.responseURL
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // Server responded with a status code that falls out of the range of 2xx
+          console.log(error.response.data)
+          console.log(error.response.status)
+          console.log(error.response.headers)
+        } else if (error.request) {
+          // Request was made but no response was received
+          console.log(error.request)
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message)
+        }
+        console.log(error)
+      })
+    },
+    getStateData () {
+      const self = this
+
+      let socParams = {
+        type: 'state',
+        tech: 'acfosw',
+        speed: '25',
+        $limit: 5,
+        $$app_token: this.appToken,
+        $select: 'id,sum(has_0),sum(has_1),sum(has_2),sum(has_3more)',
+        $group: 'id',
+        $WHERE: "starts_with(id,'" + this.$refs.autocomplete.typeaheadModel.geoid + "')"
+      }
+
+      axios
+      .get(this.socrataURL, {
+        params: socParams,
+        headers: this.httpHeaders
+      })
+      .then(function (response) {
+        self.assembleRows(response.data, 'state')
+        self.stateDataDownload = response.request.responseURL
       })
       .catch(function (error) {
         if (error.response) {
@@ -379,32 +425,49 @@ export default {
       this.rows = []
 
       for (let rdi in rawData) {
+        let areaName = ''
+        let stateName = ''
         let totalPop = parseInt(rawData[rdi].sum_has_0) + parseInt(rawData[rdi].sum_has_1) + parseInt(rawData[rdi].sum_has_2) + parseInt(rawData[rdi].sum_has_3more)
+
         if (!totalPop) totalPop = 1
 
-        let areaName = lookupData ? lookupData[rawData[rdi].id] : 'Nationwide'
+        if (lookupData === 'Nationwide') {
+          // Populate Nationwide results table data
+          let rowData = this.joinRows('Nationwide', stateName, rawData[rdi], totalPop)
+
+          this.nwData = []
+          this.nwData.push(rowData)
+        } else if (lookupData === 'state') {
+          // Populate State results table data
+          let rowData = this.joinRows('state', this.$refs.autocomplete.typeaheadModel.name, rawData[rdi], totalPop)
+
+          this.stateData = []
+          this.stateData.push(rowData)
+        } else {
+          areaName = lookupData[rawData[rdi].id]
+        }
 
         if (this.searchType !== 'CBSA (MSA)' ||
            !this.$refs.autocomplete.typeaheadModel.geoid ||
            (this.$refs.autocomplete.typeaheadModel.geoid && areaName.indexOf(this.abbreviationByGeoID[this.$refs.autocomplete.typeaheadModel.geoid]) > 0)) {
-          let rowData = {
-            area_name: areaName,
-            zero_providers: (100.0 * parseFloat(rawData[rdi].sum_has_0) / (1.0 * totalPop)).toFixed(2),
-            one_provider: (100.0 * (parseFloat(rawData[rdi].sum_has_1) + parseFloat(rawData[rdi].sum_has_2) + parseFloat(rawData[rdi].sum_has_3more)) / (1.0 * totalPop)).toFixed(2),
-            two_provider: (100.0 * (parseFloat(rawData[rdi].sum_has_2) + parseFloat(rawData[rdi].sum_has_3more)) / (1.0 * totalPop)).toFixed(2),
-            three_provider: (100.0 * parseFloat(rawData[rdi].sum_has_3more) / (1.0 * totalPop)).toFixed(2)
-          }
-
-          if (areaName === 'Nationwide') {
-            this.nwData = []
-            this.nwData.push(rowData)
-          } else {
-            this.rows.push(rowData)
-          }
+          // Populate results table based on selected geography type
+          let rowData = this.joinRows(areaName, stateName, rawData[rdi], totalPop)
+          this.rows.push(rowData)
 
           this.justMounted = false
         }
       }
+    },
+    joinRows (areaName, stateName, rawDataRDI, totalPop) {
+      let rowData = {
+        area_name: stateName === '' ? areaName : stateName,
+        zero_providers: (100.0 * parseFloat(rawDataRDI.sum_has_0) / (1.0 * totalPop)).toFixed(2),
+        one_provider: (100.0 * (parseFloat(rawDataRDI.sum_has_1) + parseFloat(rawDataRDI.sum_has_2) + parseFloat(rawDataRDI.sum_has_3more)) / (1.0 * totalPop)).toFixed(2),
+        two_provider: (100.0 * (parseFloat(rawDataRDI.sum_has_2) + parseFloat(rawDataRDI.sum_has_3more)) / (1.0 * totalPop)).toFixed(2),
+        three_provider: (100.0 * parseFloat(rawDataRDI.sum_has_3more) / (1.0 * totalPop)).toFixed(2)
+      }
+
+      return rowData
     },
     compareAreas () {
       const self = this
@@ -444,6 +507,12 @@ export default {
         }
       }
 
+      if (this.areaForSearch === 'state') {
+        this.getStateData()
+      } else {
+        this.stateData = []
+      }
+
       this.updateUrlParams()
 
       // Display spinner while chart data loads
@@ -472,6 +541,9 @@ export default {
             socLookupParams['$WHERE'] = "starts_with(geoid,'" + self.$refs.autocomplete.typeaheadModel.geoid + "')"
           }
         }
+
+        // Update download link
+        self.geogTypeDownload = response.request.responseURL
 
         axios
         .get(self.socrataLookupURL, {
@@ -570,7 +642,7 @@ export default {
 
       if (routeQP.geoid) {
         if (routeQP.geoid in this.stateGeoidToName) {
-          this.searchArea('')
+          this.searchArea('state')
           this.$refs.autocomplete.typeaheadModel = {
             'geoid': routeQP.geoid,
             'name': this.stateGeoidToName[routeQP.geoid],
@@ -591,6 +663,22 @@ export default {
     },
     openAboutAreaCompare () {
       EventHub.$emit('openAboutAreaCompare')
+    },
+    exportTable (tableID, geogType) {
+      // Export results table as Excel file
+      let tableText = "<table border='1px'><tr>"
+      let tab = document.querySelector(tableID)
+
+      for (let j = 0; j < tab.rows.length; j++) {
+        tableText += tab.rows[j].innerHTML + '</tr>'
+      }
+
+      tableText += '</table>'
+      // reomve input params
+      tableText = tableText.replace(/<input[^>]*>|<\/input>/gi, '')
+
+      let blob = new Blob([tableText], { type: 'text/xls' })
+      FileSaver.saveAs(blob, geogType + '_results.xls')
     }
   },
   computed: {
