00f95da1b51bdf0f9b3dce999ee4547a5da122ee
Nathaniel Guieb <bizuilize@gmail.com>
2018-02-20 11:13:15 -0500

    add accessibility features to About modals

--- src/components/LocationSummary/AboutLocationSummary/AboutLocationSummary.component.js
+++ src/components/LocationSummary/AboutLocationSummary/AboutLocationSummary.component.js
@@ -1,10 +1,12 @@
 import { Modal } from 'uiv'
+import { modalAccessibility } from '../../../_mixins/modal-accessibility.js'
+
 import EventHub from '../../../_mixins/EventHub.js'
 
 export default {
   name: 'AboutLocationSummary',
   components: { Modal },
-  mixins: [],
+  mixins: [modalAccessibility],
   props: [],
   mounted () {
     EventHub.$on('openAboutLocSummary', () => {
